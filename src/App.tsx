768eecc1e5485b2fb019916b0e5c24ab96c18a03
PSSAUDE\Jhonanthan.xavier <jhonanthanwork@gmail.com>
2025-12-10 13:06:40 -0300

    aplicando css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading/Heading';
+import { Body } from './components/Body/Body';
+
 export function App() {
   console.log('Hello');
   return (
     <>
-      <h1>Hello</h1>
-      <p>
-        Hello Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Accusantium nostrum et, sequi doloribus dicta, dolore autem iste
-        delectus ullam iure illo possimus eligendi aut voluptatum aspernatur
-        quas assumenda officia, in atque nulla quibusdam quaerat? Nemo et minima
-        eum, quam veniam sed quos, debitis porro voluptate asperiores numquam
-        fuga quas facilis, similique dolorum sequi aut velit fugit dolore
-        consequatur quae cum! Consequuntur voluptate laudantium quae accusamus
-        repellat dolores, consequatur maxime beatae minima quisquam sed dolor
-        repellendus illo similique tenetur natus laborum ad, enim fuga quia,
-        officia dolore provident. Ab repellat et magnam error dolorem, quisquam,
-        ratione exercitationem dolore asperiores, ea possimus.
-      </p>
+      <Heading />
+      <Body />
     </>
   );
 }
---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,17 @@
+export function Body() {
+  return (
+    <p>
+      Hello Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusantium nostrum et, sequi doloribus dicta, dolore autem iste delectus
+      ullam iure illo possimus eligendi aut voluptatum aspernatur quas assumenda
+      officia, in atque nulla quibusdam quaerat? Nemo et minima eum, quam veniam
+      sed quos, debitis porro voluptate asperiores numquam fuga quas facilis,
+      similique dolorum sequi aut velit fugit dolore consequatur quae cum!
+      Consequuntur voluptate laudantium quae accusamus repellat dolores,
+      consequatur maxime beatae minima quisquam sed dolor repellendus illo
+      similique tenetur natus laborum ad, enim fuga quia, officia dolore
+      provident. Ab repellat et magnam error dolorem, quisquam, ratione
+      exercitationem dolore asperiores, ea possimus.
+    </p>
+  );
+}
